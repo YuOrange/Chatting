c5345974146a07d0cc04b05173825de1a325e582
Your Name <1357709526@qq.com>
2018-02-27 20:30:00 +0800

    lz

--- app/src/main/java/com/chatting/MainActivity.java
+++ app/src/main/java/com/chatting/MainActivity.java
@@ -1,25 +1,31 @@
 package com.chatting;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == 0) {
+//                Intent intent = new Intent(MainActivity.this,secent.class);
+//                startActivity(intent);
+                Toast.makeText(MainActivity.this, "该跳转了", Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        TextView tv = (TextView) findViewById(R.id.all);
-        //这是第一次往上面push
-
-        //刘哲第二次上传
-        //开始觉得卡就
-        //lalala
-//        dfdfdff
-        //的jfk的jfk的jfk的jfk的就
-
+        handler.sendEmptyMessageDelayed(0, 2000);
     }
 }
