ec1dc0444d56bea7bee651aa12caa5a8abae1a76
Lei <a7776840>
2018-02-27 20:04:32 +0800

    123132

--- app/src/main/java/com/chatting/MainActivity.java
+++ app/src/main/java/com/chatting/MainActivity.java
@@ -1,11 +1,22 @@
 package com.chatting;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private Handler handler=new Handler(){
+    @Override
+    public void handleMessage(Message msg) {
+        super.handleMessage(msg);
+        if (msg.what==0) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
+    }
+};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
